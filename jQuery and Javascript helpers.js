36aa96090c3a3f7e4f80b216592124b06c87a298
Javier Abrego <abregomail@gmail.com>
2014-12-19 13:17:37 +0100

    Update jQuery and Javascript helpers.js

--- jQuery and Javascript helpers.js
+++ jQuery and Javascript helpers.js
@@ -44,3 +44,18 @@ function isApp(){
 	    // Web page
 	}	
 }
+
+/*
+where a 6-digit zerofilled representation of the number 5 would be "000005"
+*/
+function zeroFill( number, width )
+{
+  width -= number.toString().length;
+  if ( width > 0 )
+  {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // always return a string
+}
+
+
